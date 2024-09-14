befa8b1eb2f33e9e6b1ee60e3ef7ff23d9c7c24d
ONLYIWIN21 <flintthecuber@gmail.com>
2024-09-13 22:20:10 -0400

    basic setup

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,17 @@
 use dioxus::prelude::*;
 use dioxus_logger::tracing::{info, Level};
 
+mod home;
+
+use home::Home;
+
 #[derive(Clone, Routable, Debug, PartialEq)]
 enum Route {
     #[route("/")]
     Home {},
-    #[route("/blog/:id")]
-    Blog { id: i32 },
 }
 
 fn main() {
-    // Init logger
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
     info!("starting app");
     launch(App);
@@ -21,32 +22,5 @@ fn main() {
 fn App() -> Element {
     rsx! {
         Router::<Route> {}
-    }
-}
-
-#[component]
-fn Blog(id: i32) -> Element {
-    rsx! {
-        Link { to: Route::Home {}, "Go to counter" }
-        "Blog post {id}"
-    }
-}
-
-#[component]
-fn Home() -> Element {
-    let mut count = use_signal(|| 0);
-
-    rsx! {
-        Link {
-            to: Route::Blog {
-                id: count()
-            },
-            "Go to blog"
-        }
-        div {
-            h1 { "High-Five counter: {count}" }
-            button { onclick: move |_| count += 1, "Up high!" }
-            button { onclick: move |_| count -= 1, "Down low!" }
-        }
     }
 }
