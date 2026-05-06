c1b1ebe6570b6e1557510353d3a696b5944338b2
Flint Mueller <flintthecuber@gmail.com>
2026-05-06 01:17:09 -0400

    profile image

--- src/home.rs
+++ src/home.rs
@@ -1,6 +1,7 @@
 use dioxus::prelude::*;
 
 use crate::aboutme::AboutMe;
+use crate::colors::{BLUE, TEXT};
 use crate::footer::Footer;
 use crate::header::Header;
 use crate::links::Links;
@@ -8,6 +9,7 @@ use crate::projects::Projects;
 use crate::section::Section;
 
 static BACKGROUND: Asset = asset!("/assets/bg.jpeg");
+static PROFILE: Asset = asset!("/assets/me.png");
 
 #[component]
 pub fn Home() -> Element {
@@ -18,7 +20,21 @@ pub fn Home() -> Element {
             background_size: "100% auto",
             background_position: "0 10%",
             //Header {}
-            div {
+            div { display: "flex",
+                div {
+                    width: "150px",
+                    height: "200px",
+                    overflow: "hidden",
+                    border: "5px solid {BLUE}",
+                    background_color: TEXT,
+                    border_radius: "60px",
+                    img {
+                        src: PROFILE,
+                        width: "100%",
+                        height: "100%",
+                        object_fit: "cover",
+                    }
+                }
                 h1 { "Hi, I'm Flint, a sophomore studying computer science at Stony Brook University." }
             }
             Section { name: "projects", Projects {} }
